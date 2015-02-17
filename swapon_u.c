582b4f8214d708f0c74d00f4c677a23d9449527d
WHR <msl0000023508@gmail.com>
2015-02-17 12:40:27 +0800

    Try to port swapon

--- swapon_u.c
+++ swapon_u.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#ifdef __linux__
 //#include <asm/page.h>
 #include <sys/swap.h>
+#endif
 #include <string.h>
 #include <errno.h>
 
+#ifdef __linux__
 /* XXX These need to be obtained from kernel headers. See b/9336527 */
 #define SWAP_FLAG_PREFER        0x8000
 #define SWAP_FLAG_PRIO_MASK     0x7fff
 #define SWAP_FLAG_PRIO_SHIFT    0
 #define SWAP_FLAG_DISCARD       0x10000
+#else
+#define swapon(P,F) swapon(P)
+#endif
 
 void usage(const char *name) {
-	fprintf(stderr, "Usage: %s [-p prio] <filename>\n"
-			"	prio must be between 0 and %d\n", name, SWAP_FLAG_PRIO_MASK);
+	fprintf(stderr, "Usage: %s [-p prio] <filename>\n", name);
+#ifdef __linux__
+	fprintf(stderr, "	prio must be between 0 and %d\n", SWAP_FLAG_PRIO_MASK);
+#endif
 }
 
+#ifdef __linux__
 int parse_prio(const char *prio_str) {
 	unsigned long int p = strtoul(prio_str, NULL, 10);
 	return (p > SWAP_FLAG_PRIO_MASK)? -1 : (int)p;
 }
+#endif
 
 int swapon_main(int argc, char **argv) {
 	int flags = 0;
@@ -29,9 +39,14 @@ int swapon_main(int argc, char **argv) {
 	opterr = 0;
 
 	while(1) {
-		int c = getopt(argc, argv, "hp:");
+		int c = getopt(argc, argv, "h"
+#ifdef __linux__
+			"p"
+#endif
+			":");
 		if(c == -1) break;
 		switch(c) {
+#ifdef __linux__
 			case 'p':
 				if(!optarg) {
 					usage(argv[0]);
@@ -41,6 +56,7 @@ int swapon_main(int argc, char **argv) {
 				flags |= SWAP_FLAG_PREFER;
 				flags |= (prio << SWAP_FLAG_PRIO_SHIFT) & SWAP_FLAG_PRIO_MASK;
 				break;
+#endif
 			case 'h':
 				usage(argv[0]);
 				return 0;
