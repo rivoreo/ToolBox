13aebdb48b9e19da303d3ebcdf467187e0960b2d
JohnnySun <bmy001@gmail.com>
2015-03-12 17:18:20 +0800

    command more add open file support(Command still incomplete)

--- more_u.c
+++ more_u.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <errno.h>
@@ -7,6 +8,7 @@
 static struct winsize winsz;
 static int use_tty;
 static int page_col, page_row;
+static char filename[1024];
 static int usage(char *name) {
 	fprintf(stdout, "Usage:\n"
 				"%s [options] <file>...\n", name);
@@ -15,11 +17,11 @@ static int usage(char *name) {
 static int read_file(FILE *fp) {
 	char line[page_col];
 	while(fgets(line, page_col, fp) != NULL) {
-		if(page_row != 0) {
-			fputs(line,stdout);
+		if(page_row != 1) {
+			fflush(stdout);
+			fprintf(stdout,"%s",line);
 			page_row--;
-		} else {
-			//Test
+		} else {		
 			return 3;
 		}
 
@@ -28,14 +30,10 @@ static int read_file(FILE *fp) {
 }
 int more_main(int argc, char *argv[]) {
 	use_tty = isatty(STDOUT_FILENO);
-	/* If not a pipe */
-	//if(isatty(STDIN_FILENO)) {
-	//	usage(argv[0]);
-	//	return 1;
-	//}
 
 	if(use_tty) {
-		/* Get windows size 
+		/*
+		 * Get windows size 
 		 *
 		 * struct winsize {
 		 *	unsigned short ws_row;
@@ -47,18 +45,42 @@ int more_main(int argc, char *argv[]) {
 		if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &winsz) == -1) {
 			return errno;
 		}
+
 		page_row = winsz.ws_row;
 		page_col = winsz.ws_col;
+
 	} else {
 		usage(argv[0]);
 		return 1;
 	}
+	
+	/* 
+	 * Now only support open a file or pipe
+	 * So I dont use get opt now.
+	 */
 	FILE *fp;
-	if((fp = fdopen(STDIN_FILENO,"r")) == NULL) {
-		fprintf(stderr,"open file err\n");
-		return errno;
+	
+	if(argc != 1) {
+		strcpy(filename, argv[1]);
+		
+		if((fp = fopen(filename,"r")) == NULL) {
+			perror("Error open file");
+			return errno;
+		}
 	}
+	
+	/* If not a pipe */
+	if(!isatty(STDIN_FILENO)) {
+		
+		if((fp = fdopen(STDIN_FILENO,"r")) == NULL) {
+			perror("Error open STDIN_FILENO");
+			return errno;
+		}
+	}
+
+
 	read_file(fp);
+
 	return 0;
 }
 
